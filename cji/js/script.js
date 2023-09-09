a53b80023fab7ed027df6b9188462166c531772e
bdinsite <bdinsight.oy@gmail.com>
2023-09-09 16:08:56 +0300

    Add and style resize slider and button

--- cji/js/script.js
+++ cji/js/script.js
@@ -3,16 +3,34 @@ let canvasSize = 16;
 
 // CREATE ELEMENTS FOR CANVAS
 var divContainer = document.createElement('div');
+const inputContainer = document.createElement('div');
+const input = document.createElement('input');
+const textInput = document.createElement('input');
+const button = document.createElement('button');
 
 // ADD ATTRIBUTES TO ELEMENTS
 divContainer.className = 'container';
+inputContainer.className = "inputContainer";
+input.setAttribute('type', 'range');
+input.setAttribute('min', '16');
+input.setAttribute('max', '64');
+input.setAttribute('id', 'changeValue');
+input.setAttribute('onchange', 'updateTextInput(this.value);');
+textInput.setAttribute('id', 'textInput');
+textInput.setAttribute('value', '16');
+button.textContent = 'Resize Canvas';
 
 // GRAB ELEMENTS
 const main = document.querySelector('main');
 const container = document.querySelector('.container');
+const header = document.querySelector('header');
 
 // INSERT ELEMENTS
 main.appendChild(divContainer);
+header.appendChild(inputContainer);
+inputContainer.appendChild(input);
+inputContainer.appendChild(textInput);
+inputContainer.appendChild(button);
 
 // CREATE CANVAS ON STARTUP (!&x16)
 for(let i = 0; i < canvasSize; i++){
